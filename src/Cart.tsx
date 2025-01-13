ddc953412832023a05ceab1dab04b3f32183af27
divyaaaa10 <c.divya1096@gmail.com>
2025-01-13 14:24:53 +0530

    cart changes

--- src/Cart.tsx
+++ src/Cart.tsx
@@ -1,39 +1,45 @@
 import { useState } from "react"
 export default function Cart() {
-  const [quantity, setQauntity] = useState(0)
+  const [quantity, setQuantity] = useState([0, 0])
   const price = [100, 200];
   const [totalPrice, setTotalPrice] = useState(0)
 
-  function Increase() {
-    setQauntity(quantity + 1)
+  function Increase(i: number) {
+    const newQuant = [...quantity]
+    newQuant[i]++
+    setQuantity(newQuant)
   }
 
-  function Decrease() {
-    if (quantity > 0) {
-      setQauntity(quantity - 1)
+  function Decrease(i: number) {
+    const newQuant = [...quantity]
+    if (newQuant[i] > 0) {
+      newQuant[i]--
+      setQuantity(newQuant)
     }
   }
 
   function TotalPrice() {
     let tot = 0
-    tot += quantity * price
+    for (let i = 0; i < quantity.length; i++) {
+      tot += quantity[i] * price[i]
+    }
     setTotalPrice(tot)
   }
   return (
     <div>
       <div>
         <div> Item 1</div>
-        <div> Quantity = {quantity}</div>
+        <div> Quantity = {quantity[0]}</div>
         <div>Price = {price[0]}</div>
-        <button onClick={Increase}>+</button>
-        <button onClick={Decrease}>-</button>
+        <button onClick={() => Increase(0)}>+</button>
+        <button onClick={() => Decrease(0)}>-</button>
       </div>
       <div>
         <div> Item 2</div>
-        <div> Quantity = {quantity}</div>
+        <div> Quantity = {quantity[1]}</div>
         <div>Price = {price[1]}</div>
-        <button onClick={Increase}>+</button>
-        <button onClick={Decrease}>-</button>
+        <button onClick={() => Increase(1)}>+</button>
+        <button onClick={() => Decrease(1)}>-</button>
       </div>
       <div>total = {totalPrice}</div>
       <button onClick={TotalPrice}>Calculate Total</button>
