e4b57f6fc7f2ed7e86d102f4ac3282c3d6ca8e8e
divyaaaa10 <c.divya1096@gmail.com>
2025-01-13 11:28:34 +0530

    Initial Commit

---
+++ src/Cart.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react"
+export default function Cart() {
+  const [quantity, setQauntity] = useState(0)
+  const price = [100, 200];
+  const [totalPrice, setTotalPrice] = useState(0)
+
+  function Increase() {
+    setQauntity(quantity + 1)
+  }
+
+  function Decrease() {
+    if (quantity > 0) {
+      setQauntity(quantity - 1)
+    }
+  }
+
+  function TotalPrice() {
+    let tot = 0
+    tot += quantity * price
+    setTotalPrice(tot)
+  }
+  return (
+    <div>
+      <div>
+        <div> Item 1</div>
+        <div> Quantity = {quantity}</div>
+        <div>Price = {price[0]}</div>
+        <button onClick={Increase}>+</button>
+        <button onClick={Decrease}>-</button>
+      </div>
+      <div>
+        <div> Item 1</div>
+        <div> Quantity = {quantity}</div>
+        <div>Price = {price[1]}</div>
+        <button onClick={Increase}>+</button>
+        <button onClick={Decrease}>-</button>
+      </div>
+      <div>total = {totalPrice}</div>
+      <button onClick={TotalPrice}>Calculate Total</button>
+    </div>
+  )
+}
---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Cart from './Cart'
+
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <Cart />
+  </React.StrictMode>
+)
